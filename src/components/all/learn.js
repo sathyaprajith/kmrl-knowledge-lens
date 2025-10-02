a56a483869e55fbeb5189faaf86cc448582f242a
Mallela Sathya Prajith Reddy <sathyaprajith5929@gmail.com>
2025-10-02 15:09:30 +0530

    aa

--- src/components/all/learn.js
+++ src/components/all/learn.js
@@ -1,14 +1,11 @@
-function fetchData(){
-    return new Promise((resolve,reject) => {
-
-    });
-    }
-async function fetchDataData(){
-        try {
-           console.log("Data Fetched"); 
-        } catch (error) {
-            console.log("Unable to Fetch data");
-            
-        }
+function fetchData() {
+  return new Promise((resolve, reject) => {});
+}
+async function fetchDataData() {
+  try {
+    console.log("Data Fetched");
+  } catch (error) {
+    console.log("Unable to Fetch data");
+  }
 }
 getData();
